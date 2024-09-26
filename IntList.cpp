a26f381776eaf4510031b1b3828ae1f32cfc5366
EndlessFashion <156638497+EndlessFashion@users.noreply.github.com>
2024-09-26 13:52:20 -0700

    Add files via upload

---
+++ IntList.cpp
@@ -0,0 +1,69 @@
+#include "IntList.h"
+
+#include <ostream>
+
+using namespace std;
+
+IntList::IntList() : head(nullptr) {}
+
+
+void IntList::push_front(int val) {
+   if (!head) {
+      head = new IntNode(val);
+   } else {
+      IntNode *temp = new IntNode(val);
+      temp->next = head;
+      head = temp;
+   }
+}
+ostream& operator<<(ostream& os, const IntList& list)
+{
+    if (list.head == nullptr)
+    {
+        return os;
+    }
+
+    os << list.head->value;
+    return operator<<(os, list.head->next);
+}
+bool IntList::exists(int val) const
+{
+    if (head == nullptr)
+    {
+        return false;
+    }
+    
+    return exists(head, val);
+}
+bool IntList::exists(IntNode* node, int val) const
+{
+    if (node->value != val)
+    {
+        if (node->next == nullptr)
+        {
+            return false;
+        }
+        else
+        {
+            return exists(node->next, val);
+        }
+    }
+    else
+    {
+        return true;
+    }
+}
+ostream& operator<<(ostream& os, IntNode* node)
+{
+    if (node == nullptr)
+    {
+        return os;
+    }
+    else
+    {
+        os << " ";
+    }
+
+    os << node->value;
+    return operator<<(os, node->next);
+}
